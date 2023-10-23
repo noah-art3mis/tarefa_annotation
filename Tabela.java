50d20db4987f18924d728850d094a86da76664c3
noah-art3mis <costa.araujo.gustavo@gmail.com>
2023-10-23 15:59:19 -0300

    first commit

---
+++ Tabela.java
@@ -0,0 +1,14 @@
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Tabela
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Tabela {
+  String value();
+}
